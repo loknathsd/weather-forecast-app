4c55219acc87e43cfaba257e7e0d476f34ed4716
loknathsd <loknathsds66@gmail.com>
2023-06-23 20:37:11 +0600

    current day and next few days data show

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
 
-const Forecast = ({forecast}) => {
-    console.log(' forecast' , forecast)
+const Forecast = ({ forecast }) => {
+    const today = new Date();
+    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    // Calculate the next 3 weekdays
+    const nextWeekdays = [];
+    for (let i = 1; nextWeekdays.length < 3; i++) {
+        const nextDay = new Date(today);
+        nextDay.setDate(today.getDate() + i);
+        const weekDay = nextDay.getDay();
+        if (weekdays.includes(weekdays[weekDay])) {
+            nextWeekdays.push(weekdays[weekDay]);
+        }
+    }
+
+    // Filter forecast data for the next 3 weekdays and remove duplicates
+    const filteredForecast = forecast.filter((item) => {
+        const forecastDate = new Date(item.dt * 1000);
+        const forecastWeekday = weekdays[forecastDate.getDay()];
+
+        return nextWeekdays.includes(forecastWeekday);
+    });
+
+    // Remove duplicates from the filtered forecast
+    const uniqueForecast = [];
+    filteredForecast.forEach((item) => {
+        const forecastDate = new Date(item.dt * 1000);
+        const forecastWeekday = weekdays[forecastDate.getDay()];
+        if (!uniqueForecast.some((uniqueItem) => uniqueItem.weekday === forecastWeekday)) {
+            uniqueForecast.push({ weekday: forecastWeekday, item });
+        }
+    });
+
     return (
         <div>
-            
+            <h2>Forecast for the Next 3 Weekdays</h2>
+            <div className='flex justify-center mt-10 gap-4'>
+                {uniqueForecast.map((item) => {
+                    const { weekday, item: forecastItem } = item;
+                    return (
+                        <div className='border border-red-400 w-52 ' key={forecastItem.dt}>
+                            <p>Weekday: {weekday}</p>
+                            <p>  Condition: {forecastItem.weather[0].main}</p>
+                            <p> Description: {forecastItem.weather[0].description}</p>
+                            <p> Temperature: {forecastItem.main.temp}°C</p>
+                            <p> Humidity: {forecastItem.main.humidity}% </p>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
--- src/components/WeatherInfo/WeatherInfo.jsx
+++ src/components/WeatherInfo/WeatherInfo.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
-const WeatherInfo = ({weather}) => {
-    console.log(weather)
-    return (
-        <div>
-          weather information detail for every country 
-        </div>
-    );
+const WeatherInfo = ({ weather }) => {
+  const { name, main, weather: weatherData } = weather;
+  console.log(weather)
+
+  // Get the current weekday and date
+  const today = new Date();
+  const options = { weekday: 'long', month: 'long', day: 'numeric' };
+  const currentDate = today.toLocaleDateString('en-US', options);
+  const temperatureCelsius = Math.round(main.temp - 273.15);
+
+  return (
+    <div>
+      <h2>Current Weather</h2>
+      <div>
+        <h3>{name}</h3>
+        <p>{currentDate}</p>
+        <p>Temperature: {temperatureCelsius}°C</p>
+        <p>Humidity: {main.humidity}%</p>
+        <p>Condition: {weatherData[0].main}</p>
+        <p>Description: {weatherData[0].description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default WeatherInfo;
